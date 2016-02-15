35fc2e312844a2ff90dcb342dff7eb1aafeff1e5
AvalancheYT <darkeagle11221@gmail.com>
2016-02-15 20:44:01 +1100

    Fixed

--- src/server/AvalancheYT/FreedomCore/FreedomCore.java
+++ src/server/AvalancheYT/FreedomCore/FreedomCore.java
@@ -2,16 +2,13 @@ package server.AvalancheYT.FreedomCore;
 
 import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
-<<<<<<< HEAD
 import server.AvalancheYT.FreedomCore.Commands.Command_admin;
 import server.AvalancheYT.FreedomCore.Commands.Command_fc;
 import server.AvalancheYT.FreedomCore.Commands.Command_list;
 import server.AvalancheYT.FreedomCore.Commands.Command_op;
-=======
 import server.AvalanceYT.FreedomCore.Commands.Command_fc;
 import server.AvalanceYT.FreedomCore.Commands.Command_list;
 import server.AvalanceYT.FreedomCore.Commands.Command_op;
->>>>>>> origin/master
 import server.AvalancheYT.FreedomCore.Config.ConfigManager;
 
 public class FreedomCore extends JavaPlugin {
@@ -37,10 +34,7 @@ public class FreedomCore extends JavaPlugin {
         new ConfigManager();
         this.getCommand("list").setExecutor(new Command_list(this));
         this.getCommand("fc").setExecutor(new Command_fc(this));
-<<<<<<< HEAD
         this.getCommand("admin").setExecutor(new Command_admin(this));
-=======
->>>>>>> origin/master
         this.getCommand("op").setExecutor(new Command_op(this));
     }
 }
